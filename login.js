b247cd261a7269f887c9636583e96dddb68e9b77
Khurram666 <35162412+Khurram666@users.noreply.github.com>
2018-03-24 14:10:23 +0500

    Add files via upload

--- login.js
+++ login.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react'
 
+
+
+
 class App extends Component {
+constructor(props) {
+    super(props);
+    this.state = {value: ''};
+}
+ handleChange=(event)=> {
+    this.setState({value: event.target.value});
+  }
+
   render() {
     return (
+      
       <div>
 
 
         <div className="container">
           <div className="wrapper">
-            <form action="" method="post" name="Login_Form" class="form-signin">
-              <h3 className="form-signin-heading">Welcome Back! Please Sign In</h3>
+            <form action="" method="post" name="Login_Form" className="form-signin">
+              <h3>Welcome Back! Please Sign In</h3>
               <hr className="colorgraph" />
               <br/>
-
-                <input type="text" className="form-control" name="Username" placeholder="Username" required autofocus />
-                <input type="password" className="form-control" name="Password" placeholder="Password" required />
+                <input type="text" className="form-control" name="Username" placeholder="Username" required  value={this.state.value} onChange={this.handleChange}/>
+                <input type="password" className="form-control" name="Password" placeholder="Password" required value={this.state.value} onChange={this.handleChange} />
 
                 <button className="btn btn-lg btn-success btn-block" name="Submit" value="Login" type="Submit">Login</button>
-                <button className="btn btn-lg btn-info btn-block" name="Submit" value="Login" type="Submit">Registor</button>  			  			
+                
 		</form>			
 	</div>
 </div>
